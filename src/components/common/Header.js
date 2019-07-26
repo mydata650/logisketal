9afecac3b1477eac794f6dea718a6174e5746df1
mydata650 <mydata650@gmail.com>
2019-07-26 12:06:37 +0200

    LeotalCookieMsgCommit

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
     const activeLink = { color: "/F15B2A" }
     return (
         <nav className="navbar navbar-expand-lg navbar-light bgc1">
-            <a className="navbar-brand" href="/">logisketal</a>
+            <a className="navbar-brand" href="/">Loetal</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
